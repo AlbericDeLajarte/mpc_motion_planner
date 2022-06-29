3eee46d46e1cfca6fdd7b2a3bfe9473da3ac8c11
AlbericLaj <albericlajarte@gmail.com>
2022-06-29 16:08:55 +0200

    using realistic target velocity

--- src/main.cpp
+++ src/main.cpp
@@ -76,20 +76,20 @@ int main(int, char**) {
     mpc_t::state_t final_state; 
     
     // Search over target configuration until one is inside joint limits
-    int nTry = 0;
-    while (feasibleTarget == false && nTry < 100){
-        qTarget = robot.inverse_kinematic(Matrix3d::Identity(), Vector3d(0.4, 0., 0.5));
+    // int nTry = 0;
+    // while (feasibleTarget == false && nTry < 100){
+    //     qTarget = robot.inverse_kinematic(Matrix3d::Identity(), Vector3d(0.4, 0., 0.5));
 
-        // Final state
-        // final_state.head(7) = qTarget;
+    //     // Final state
+    //     // final_state.head(7) = qTarget;
 
-        // Check state constraints violation
-        if( (final_state.array() < ubx.array()).all() && (final_state.array() > lbx.array()).all() ){
-            std::cout << "Solved in " << nTry << " trials. Initial state: " << std::endl;
-            feasibleTarget = true;
-        }
-        nTry ++;
-    }
+    //     // Check state constraints violation
+    //     if( (final_state.array() < ubx.array()).all() && (final_state.array() > lbx.array()).all() ){
+    //         std::cout << "Solved in " << nTry << " trials. Initial state: " << std::endl;
+    //         feasibleTarget = true;
+    //     }
+    //     nTry ++;
+    // }
 
     // qTarget << 3.14*0.5, -0.936495238095238, -2.3686368421052633, -0.9453, 2.368636842105263, 2.9079545454545457, 0.4281842865538632;
     // final_state.head(7) = qTarget;
@@ -99,10 +99,12 @@ int main(int, char**) {
     // // Compute desired final joint speed from cartesian [linear, angular] speed
     // final_state.tail(7) = robot.inverse_velocities(qTarget, Vector3d(0.5, 0., 0.3), Vector3d(0.0, 0.0, 0.0));
 
-    for(int iter =0; iter<1000; iter++){
-
+    for(int iter =0; iter<1; iter++){
+    
+    std::srand((unsigned int) time(0));
     final_state.head(7) = 0.5*(Matrix<double, 7, 1>::Random().array()*(robot.max_position-robot.min_position).array() + (robot.max_position+robot.min_position).array() );
-    final_state.tail(7) = Matrix<double, 7, 1>::Random().array()*robot.max_velocity.array();
+    // final_state.tail(7) = Matrix<double, 7, 1>::Random().array()*robot.max_velocity.array();
+    final_state.tail(7) = robot.inverse_velocities(final_state.head(7), Matrix<double, 3, 1>::Random()*robot.max_linear_velocity, Vector3d(0.0, 0.0, 0.0));
 
     Matrix<double, 6, 1> task_velocity = robot.forward_velocities(final_state.head(7), final_state.tail(7));
     
@@ -124,6 +126,11 @@ int main(int, char**) {
         std::cout << "Angular Vel: " << task_velocity.tail(3).norm();
         task_velocity = robot.forward_velocities(final_state.head(7), final_state.tail(7));
         std::cout << " corrected to : " << task_velocity.tail(3).norm() << std::endl;
+    }
+
+    if( (final_state.tail(7).array().abs() > robot.max_velocity.array()).any() ){ 
+        // std::cout << "RK: Jerk limit of: " << jerk.transpose() << " at time: " << time << std::endl;
+        final_state.tail(7) /= 1.1*(final_state.tail(7).array().abs()/robot.max_velocity.array()).maxCoeff();
     }
 
     
@@ -275,11 +282,11 @@ int main(int, char**) {
 
             if(task_velocity.head(3).norm() > robot.max_linear_velocity) {
                 linear_vel_flag_rk = 0;
-                // std::cout << "RK: Linear Vel limit of: " << task_velocity.head(3).norm() << " at time: " << time << std::endl;
+                std::cout << "RK: Linear Vel limit of: " << task_velocity.head(3).norm() << " at time: " << time << std::endl;
             }
             if(task_velocity.tail(3).norm() > robot.max_angular_velocity) {
                 angular_vel_flag_rk = 0;
-                // std::cout << "RK: Angular Vel limit of: " << task_velocity.tail(3).norm() << " at time: " << time << std::endl;
+                std::cout << "RK: Angular Vel limit of: " << task_velocity.tail(3).norm() << " at time: " << time << std::endl;
             }
             
             logFile << time << " " 
@@ -320,11 +327,11 @@ int main(int, char**) {
 
             if(task_velocity.head(3).norm() > robot.max_linear_velocity) {
                 linear_vel_flag_mpc = 0;
-                // std::cout << "MPC: Linear Vel limit of: " << task_velocity.head(3).norm() << " at time: " << time << std::endl;
+                std::cout << "MPC: Linear Vel limit of: " << task_velocity.head(3).norm() << " at time: " << time << std::endl;
             }
             if(task_velocity.tail(3).norm() > robot.max_angular_velocity) {
                 angular_vel_flag_mpc = 0;
-                // std::cout << "MPC: Angular Vel limit of: " << task_velocity.tail(3).norm() << " at time: " << time << std::endl;
+                std::cout << "MPC: Angular Vel limit of: " << task_velocity.tail(3).norm() << " at time: " << time << std::endl;
             }
 
             logFile << time*mpc.solution_p()[0] << " " 
